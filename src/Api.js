794928d657f3377d73df0e8f0a7d7ad3bbb7b65d
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-04-17 05:14:36 +0700

    make progress

---
+++ src/Api.js
@@ -0,0 +1,5 @@
+export const apiURl = 'https://tubes3-dna-matcher.herokuapp.com/'
+
+export const Endpoints = {
+  addPenyakit: `${apiURl}/jenis_penyakit`,
+}
--- src/component/AddPenyakit.jsx
+++ src/component/AddPenyakit.jsx
@@ -1,47 +1,61 @@
 import React from 'react';
-
+import { Endpoints } from '../Api'
 import "./AddPenyakit.scss"
 
 class AddPenyakit extends React.Component {
   
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = {
+      nama_penyakit: '',
+      rantai_dna: ''
+    };
     
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+      
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
     
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <ul>
-              <label>
-                nama penyakit
-                <input type="text"/>
-              </label>
-            </ul>
-            <ul>
-              <label>
-                rantai dna
-                <input type="file" value = {this.state.value} onChange={this.handleChange} />
-              </label>
-            </ul>
-            <ul>
-              <input type="submit" value="Submit" />
-            </ul>
-          </form>
-        );
-      }
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await fetch(Endpoints.addPenyakit, {
+        method: 'POST',
+        body: JSON.stringify({ }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        })
+        const { success, errors = [] } = await res.json()
+          setErrors(errors)
+    } catch (e) {
+      setErrors([e.toString()]);
+    }
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          <label>
+            nama penyakit
+            <input type="text" required="true"/>
+          </label>
+        </ul>
+        <ul>
+          <label>
+            rantai dna
+            <input type="file" value = {this.state.value} required="true" onChange={this.handleChange} />
+          </label>
+        </ul>
+        <ul>
+          <input type="submit" value="Submit" />
+        </ul>
+      </form>
+    );
+  }
 }
-
 export default AddPenyakit;
