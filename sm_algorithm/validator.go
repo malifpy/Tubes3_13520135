8d6bd37244c57c4de3d3300ea486d041b10fbc8c
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-04-16 18:29:08 +0700

    make progress

--- sm_algorithm/validator.go
+++ sm_algorithm/validator.go
@@ -5,15 +5,15 @@ import (
 )
 
 func IsValid(s string) (bool, error) {
-    var dnaSanitized bool
-    var err error
+	var dnaSanitized bool
+	var err error
 
-    // Gak menerima string kosong
-    if s == "" {
-        return false, nil
-    }
+	// Gak menerima string kosong
+	if s == "" {
+		return false, nil
+	}
 
-    // Apakah ada yang bukan ACGT
-    dnaSanitized, err = regexp.MatchString("[^ACGT]", s)
-    return (!dnaSanitized), err
+	// Apakah ada yang bukan ACGT
+	dnaSanitized, err = regexp.MatchString("[^ACGT]", s)
+	return (!dnaSanitized), err
 }
