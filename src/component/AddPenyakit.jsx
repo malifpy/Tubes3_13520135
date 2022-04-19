7ad8dd5903f0da1f6a225f78922fc0d87923a289
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-04-19 10:05:28 +0700

    update

--- src/component/AddPenyakit.jsx
+++ src/component/AddPenyakit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Endpoints } from '../Api'
+import axios from 'axios';
+// import { Endpoints } from '../Api'
 import "./AddPenyakit.scss"
 
 class AddPenyakit extends React.Component {
@@ -7,33 +8,38 @@ class AddPenyakit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nama_penyakit: '',
-      rantai_dna: ''
+      nama_penyakit:'',
+      rantai_dna:''
     };
     
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
       
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange (event) {
+    this.setState({nama_penyakit: event.target.nama_penyakit});
+    this.setState({rantai_dna: event.target.rantai_dna});
   }
     
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    try {
-      const res = await fetch(Endpoints.addPenyakit, {
-        method: 'POST',
-        body: JSON.stringify({ }),
-            headers: {
-              'Content-Type': 'application/json',
-            },
-        })
-        const { success, errors = [] } = await res.json()
-          setErrors(errors)
-    } catch (e) {
-      setErrors([e.toString()]);
-    }
+  handleSubmit = () => {
+    const formData = new FormData();
+    
+    // Update the formData object
+    console.log(this.nama_penyakit);
+    console.log(this.rantai_dna);
+    
+    formData.append(
+      this.nama_penyakit,
+      this.rantai_dna
+    );
+    
+    // Details of the uploaded file
+    console.log(this.state.nama_penyakit);
+    console.log(this.state.rantai_dna);
+    
+    // Request made to the backend api
+    // Send formData object
+    axios.post(`https://tubes3-dna-matcher.herokuapp.com/jenis_penyakit`, formData);
   }
   
   render() {
@@ -42,17 +48,17 @@ class AddPenyakit extends React.Component {
         <ul>
           <label>
             nama penyakit
-            <input type="text" required="true"/>
+            <input type="text" required={true} value = {this.nama_penyakit} onChange={this.handleChange}/>
           </label>
         </ul>
         <ul>
           <label>
             rantai dna
-            <input type="file" value = {this.state.value} required="true" onChange={this.handleChange} />
+            <input type="text" required={true} value = {this.rantai_dna} onChange={this.handleChange} />
           </label>
         </ul>
         <ul>
-          <input type="submit" value="Submit" />
+          <button onClick={this.handleSubmit}>Submit</button>
         </ul>
       </form>
     );
