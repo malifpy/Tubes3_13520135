d0847ddd3a1ff4fa6081ea5ca7b31894bca4f9df
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-04-26 11:12:22 +0700

    update frontend

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 //export const apiURl = 'https://cors-anywhere.herokuapp.com/https://tubes3-dna-matcher.herokuapp.com'
-export const apiURL = 'http://localhost:8080'
-// export const apiURL = 'https://tubes3-dna-matcher.herokuapp.com'
+// export const apiURL = 'http://localhost:8080'
+export const apiURL = 'https://tubes3-dna-matcher.herokuapp.com'
 
 export const Endpoints = {
   addPenyakit: `${apiURL}/jenis_penyakit`,
